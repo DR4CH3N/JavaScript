9a1874def3dc86f7c41cd11ac97c42da30a5d300
DR4CH3N <thehellc4t@gmail.com>
2022-04-29 09:56:29 -0300

    adicionado um conversor de reais para dollar

---
+++ EX01-conversor/conversor.js
@@ -0,0 +1,58 @@
+/* ======================= SOLUÇÃO 1 ==========================
+
+
+ // declarando a função que é chamada quando clicamos no botão converter do HTML
+ function converteDollar() {
+
+    // pegando o valor digitado no campo valorDollar do HTML
+    let valorDollar = document.getElementById ('valorDollar').value;
+
+    // converte o valor digitado considerando o valor do dollar
+    let converteReal = valorDollar * 4.97;
+
+    //imprime o resultado da conversão na tela
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `o valor convertido em reais é R$ ${converteReal}`;
+
+    // mudar a cor para vermelho
+
+    resultado.style.color = "red";
+} 
+*/
+
+// ====================== SOLUÇÃO 2 =============================
+
+/* function converteDollar() {
+
+    // pegando o valor digitado no campo valorDollar do HTML
+    let valorDollar = document.getElementById ('valorDollar').value;
+
+    // converte o valor digitado considerando o valor do dollar
+    let cotacaodollar = document.getElementById('cotacaoDollar').value
+    let converteReal = valorDollar * 4.97;
+
+    // imprime o resultado da conversão na tela
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `o valor convertido em reais é R$ ${converteReal}`;
+
+    // mudar a cor para vermelho
+
+    resultado.style.color = "red";
+
+} */
+
+// ========================== SOLUÇÃO 3 =============================
+
+let formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function(event) {
+
+    // previne o comportamento padrão do formulario (ao enviar) 
+    event.preventDefault();
+
+    let cotacao = document.getElementById('cotacao')
+
+    
+});
+
+ 
