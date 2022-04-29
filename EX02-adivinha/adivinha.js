d199ad1c357594407f8a5c745edd52e5dcd521a8
DR4CH3N <thehellc4t@gmail.com>
2022-04-29 11:57:20 -0300

    melhorias no jogo de adivinha
    adicionado um resultado do palpite visivel ao usuario

---
+++ EX02-adivinha/adivinha.js
@@ -0,0 +1,26 @@
+let formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    // parseInt transforma um numero quebrado em numero inteiro
+    // math.random() * numero pega um numero aleatorio, multiplique ele pelo numero limite desejado 
+    // EX: quero um numero de 0 a 11
+    // voce no caso iria digitar 12 em vez de 11 
+    // sendo que se voce digitasse 11 o numero em questao nunca iria ser sorteado, apenas 10 pra baixo.
+    let numeroEscolhido = parseInt (Math.random() * 11);
+
+    let palpiteUsuario = parseInt( document.getElementById('palpite').value );
+    let resultado = document.getElementById('resultado');
+
+    if (palpiteUsuario == numeroEscolhido) {
+    resultado.innerHTML = "voce acertou! parabéns!";
+
+    } else if (palpiteUsuario < 0 || palpiteUsuario > 10) {
+    resultado.innerHTML = "digite um numero entre 0 e 10";
+    }
+    else {
+    resultado.innerHTML = "ERROU!";
+};
+});
